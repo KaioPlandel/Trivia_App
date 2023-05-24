8d2c8b3e412df5b85cb9dc0a830b08c00fa9f51b
Kaio Plandel <kaioplandel@gmail.com>
2023-05-23 21:32:49 -0300

    feat: add viewModel

---
+++ app/src/main/java/com/example/trivia/component/Questions.kt
@@ -0,0 +1,78 @@
+package com.example.trivia.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextIndent
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.trivia.screens.QuestionsViewModel
+import com.example.trivia.util.AppColors
+
+@Composable
+fun Questions(viewModel: QuestionsViewModel) {
+    val questions = viewModel.data.value.data?.toMutableList()
+
+    if (viewModel.data.value.loading == true) {
+        CircularProgressIndicator()
+    }
+}
+
+@Composable
+fun QuestionDisplay() {
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(4.dp), color = AppColors.mDarkPurple
+    ) {
+        Column(
+            modifier = Modifier.padding(12.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start
+        ) {
+            QuestionTracker()
+        }
+    }
+}
+
+@Composable
+fun QuestionTracker(counter: Int = 10, outOff: Int = 100) {
+    Text(
+        text = buildAnnotatedString {
+            withStyle(style = ParagraphStyle(textIndent = TextIndent.None)) {
+                withStyle(
+                    style = SpanStyle(
+                        color = AppColors.mLightGray,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 27.sp
+                    )
+                ) {
+                    append("Counter: $counter/")
+                    withStyle(
+                        style = SpanStyle(
+                            color = AppColors.mLightGray,
+                            fontWeight = FontWeight.Light,
+                            fontSize = 14.sp
+                        )
+                    ) {
+                        append("$outOff")
+                    }
+                }
+            }
+        },
+        modifier = Modifier.padding(20.dp)
+    )
+
+}
